849c84d30db4af36dfcab8d404e8d020cab5633f
bruno <brunolealmaciel@gmail.com>
2023-05-14 22:18:04 -0300

    parte_4

--- src/util/token.js
+++ src/util/token.js
@@ -4,14 +4,17 @@ const checktoken = async(token,id ,key) => jwt.verify(token, key, (err, decoded)
 
 })
 
+
 const setToken = async (id, key) => {
     console.log(id)
     if(id){
-        return jwt.sign({id}, key,{expiresIn:28800})
+        return jwt.sign({id}, key, { expiresIn: 28800 })
     }
     return false
 }
 
 module.exports = {
-    checktoken, setToken
+    checktoken, 
+    setToken,
+
 }
